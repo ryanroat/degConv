8bb13d84f06902b1603e5b5688025a199705e989
Ryan Roat <ryanroat@gmail.com>
2019-01-03 15:25:56 -0500

    added basic styling

--- convert.js
+++ convert.js
@@ -1,6 +1,6 @@
 function convert() {
   let tempIn = document.getElementById("tempIn").value;
-  console.log(tempIn);
+  // console.log(tempIn);
   let tempConv = Math.round((tempIn - 32) / 9 * 5);
   let tempOut = document.getElementById("tempOut");
   tempOut.innerText = tempConv
