77a4175e1afbf0a214af27b471d995a2e3024a71
Ryan Roat <ryanroat@gmail.com>
2019-01-03 19:17:18 -0500

    logic for scale labels

--- convert.js
+++ convert.js
@@ -4,12 +4,14 @@ let inScale = document.getElementById("inScale");
 let outScale = document.getElementById("outScale");
 
 inScale.innerText = selIn.value + "º"
-outScale.innerText = selOut.value + "º"
+// outScale.innerText = selOut.value + "º"
 
 function convert() {
+  inScale.innerText = selIn.value + "º"
   let tempIn = document.getElementById("tempIn").value;
   // console.log(tempIn);
   let tempConv = Math.round((tempIn - 32) / 9 * 5);
   let tempOut = document.getElementById("tempOut");
   tempOut.innerText = tempConv
+  outScale.innerText = selOut.value + "º"
 }
